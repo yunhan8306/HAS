f418adfac6df694ca580c70ea04f908f8680fbc5
yunhan8306 <dbsgks17@naver.com>
2024-05-03 23:38:27 +0900

    Feat: add Application

--- app/src/main/java/com/myStash/MainActivity.kt
+++ app/src/main/java/com/myStash/MainActivity.kt
@@ -3,8 +3,9 @@ package com.myStash
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
