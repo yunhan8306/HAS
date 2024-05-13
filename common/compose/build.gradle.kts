cf2002f55d49d58193c957cfa1d671c804b15465
yunhan <dbsgks17@naver.com>
2024-05-13 17:58:24 +0900

    Feat: add init common

---
+++ common/compose/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("myStash.android.library")
+    id("myStash.android.library.compose")
+    id("myStash.android.hilt")
+}
+
+android {
+    namespace = "com.myStash.common.compose"
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.compose.lifecycle.runtime)
+    implementation(libs.androidx.compose.lifecycle.viewmodel)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material.compose)
+    implementation(libs.androidx.lifecycle.process)
+    implementation(libs.coil)
+    implementation(libs.orbit.compose)
+    implementation(libs.orbit.viewmodel)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
