1adce6e3eea56932e667883065667d67ec669a4d
yunhan8306 <dbsgks17@naver.com>
2024-05-25 23:11:50 +0900

    Feat: add test tag more

--- feature/essential/src/main/java/com/myStash/android/feature/essential/EssentialScreen.kt
+++ feature/essential/src/main/java/com/myStash/android/feature/essential/EssentialScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -18,14 +19,18 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text2.input.TextFieldState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -33,6 +38,7 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -118,6 +124,12 @@ fun EssentialScreen(
 
     val tagScrollState = rememberLazyListState()
 
+    val scroll = rememberScrollState()
+
+    var testToggle by remember {
+        mutableStateOf(true)
+    }
+
     LaunchedEffect(tagTotalList) {
         tagScrollState.scrollToItem(0)
     }
@@ -125,8 +137,9 @@ fun EssentialScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .background(Color.White)
+            .verticalScroll(scroll),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Spacer(modifier = Modifier.height(15.dp))
         Text(
@@ -142,9 +155,22 @@ fun EssentialScreen(
         Spacer(modifier = Modifier.height(15.dp))
         Text(
             text = "Total Tag",
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { testToggle = !testToggle },
             textAlign = TextAlign.Center
         )
+        LazyColumn(
+            modifier = Modifier.height(if(testToggle) 50.dp else 200.dp)
+        ) {
+            items(10) {
+                Box(
+                    modifier = Modifier
+                        .size(20.dp)
+                        .background(if (it % 2 == 1) Color.Blue else Color.Black)
+                )
+            }
+        }
         Spacer(modifier = Modifier.height(15.dp))
         LazyRow(
             modifier = Modifier
@@ -213,9 +239,12 @@ fun EssentialScreen(
         Spacer(modifier = Modifier.height(15.dp))
         LazyVerticalGrid(
             modifier = Modifier
+//                .height(600.dp)
+                .heightIn(max = 4000.dp)
                 .fillMaxWidth()
                 .padding(horizontal = 30.dp)
-                .background(Color.Green),
+                .background(Color.Green)
+            ,
             columns = GridCells.Fixed(2),
             horizontalArrangement = Arrangement.spacedBy(20.dp),
             verticalArrangement = Arrangement.spacedBy(20.dp)
