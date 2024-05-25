12ad7ec578ab217fdbc1ef4c143a97992aef866c
yunhan8306 <dbsgks17@naver.com>
2024-05-26 02:11:11 +0900

    Feat: add SearchTextField

---
+++ core/design-system/src/main/java/com/myStash/android/design_system/ui/SearchTextField.kt
@@ -0,0 +1,46 @@
+package com.myStash.android.design_system.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text2.BasicTextField2
+import androidx.compose.foundation.text2.input.TextFieldState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SearchTextField(
+    modifier: Modifier = Modifier,
+    textState: TextFieldState,
+) {
+    BasicTextField2(
+        modifier = modifier
+            .height(40.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(Color(0xFFF5F5F5)),
+        state = textState,
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+        textStyle = TextStyle(
+            color = Color.Black,
+            fontSize = 17.sp
+        ),
+        decorator = { innerTextField ->
+            Box(
+                modifier = Modifier.padding(start = 12.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                innerTextField()
+            }
+        },
+    )
+}
