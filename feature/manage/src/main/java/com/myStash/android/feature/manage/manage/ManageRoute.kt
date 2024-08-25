730d6f81afdc0fb4c00d8a87ad90d3ba0696597c
yunhan8306 <dbsgks17@naver.com>
2024-08-26 02:46:53 +0900

    Refactor: manage
    - focus clear

--- feature/manage/src/main/java/com/myStash/android/feature/manage/manage/ManageRoute.kt
+++ feature/manage/src/main/java/com/myStash/android/feature/manage/manage/ManageRoute.kt
@@ -1,18 +1,33 @@
 package com.myStash.android.feature.manage.manage
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.myStash.android.feature.manage.tag.ManageTagRoute
+import com.myStash.android.feature.manage.type.ManageTypeAction
 import com.myStash.android.feature.manage.type.ManageTypeRoute
+import com.myStash.android.feature.manage.type.ManageTypeViewModel
 import org.orbitmvi.orbit.compose.collectAsState
 
 @Composable
 fun ManageRoute(
     viewModel: ManageViewModel = hiltViewModel(),
+    manageTypeViewModel: ManageTypeViewModel = hiltViewModel(),
     onBack: () -> Unit
 ) {
     val state by viewModel.collectAsState()
+
+    LaunchedEffect(state.selectedTab) {
+        when(state.selectedTab) {
+            ItemTab.TYPE -> {
+
+            }
+            ItemTab.TAG -> {
+                manageTypeViewModel.onAction(ManageTypeAction.FocusType(null))
+            }
+        }
+    }
 
     ManageScreen(
         state = state,
