89c0b6e6d94faaa5ec1d2ee99953806f1d6def7a
yunhan <dbsgks17@naver.com>
2024-09-06 18:37:28 +0900

    Feat: init has balloon

--- core/design-system/src/main/java/com/myStash/android/design_system/ui/component/has/HasMainItem.kt
+++ core/design-system/src/main/java/com/myStash/android/design_system/ui/component/has/HasMainItem.kt
@@ -1,6 +1,8 @@
 package com.myStash.android.design_system.ui.component.has
 
+import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
@@ -22,21 +24,35 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import coil.compose.SubcomposeAsyncImage
 import com.myStash.android.common.resource.R
 import com.myStash.android.common.util.isNotNull
 import com.myStash.android.core.model.Has
 import com.myStash.android.core.model.Tag
+import com.myStash.android.design_system.ui.color.Black
 import com.myStash.android.design_system.ui.color.ColorFamilyGray300AndGray400
 import com.myStash.android.design_system.ui.color.Purple
 import com.myStash.android.design_system.ui.color.White
+import com.myStash.android.design_system.ui.component.HasBalloon
+import com.myStash.android.design_system.ui.component.rememberHasBalloonBuilder
 import com.myStash.android.design_system.ui.component.tag.TagHasChipItem
 import com.myStash.android.design_system.ui.component.text.HasFontWeight
 import com.myStash.android.design_system.ui.component.text.HasText
+import com.myStash.android.design_system.ui.theme.clickableNoRipple
 import com.myStash.android.design_system.util.ShimmerLoadingAnimation
+import com.myStash.android.design_system.util.clickableRipple
+import com.myStash.android.design_system.util.singleClick
+import com.skydoves.balloon.compose.Balloon
+import com.skydoves.balloon.compose.rememberBalloonBuilder
+import com.skydoves.balloon.compose.setBackgroundColor
 import kotlinx.coroutines.delay
 
 @Composable
@@ -50,6 +66,18 @@ fun HasMainItem(
     onEditHas: (Has) -> Unit,
     onDeleteHas: (Has) -> Unit
 ) {
+    val hasBalloon = rememberHasBalloonBuilder()
+    var balloonEvent by remember { mutableStateOf(TimeLineMenuEvent.NONE) }
+    val builder = rememberBalloonBuilder {
+        arrowSize = 0
+        setPadding(4)
+        setMarginLeft(90)
+        setBackgroundColor(Color.Transparent)
+        setOnBalloonDismissListener {
+            balloonEvent = TimeLineMenuEvent.NONE
+        }
+    }
+
     var shortClick by remember { mutableStateOf(false) }
 
     LaunchedEffect(shortClick) {
@@ -59,79 +87,157 @@ fun HasMainItem(
         }
     }
 
-    Box(
-        modifier = Modifier
-            .border(
-                width = if(selectedNumber.isNotNull()) 2.dp else 1.dp,
-                color = if(selectedNumber.isNotNull()) Purple else ColorFamilyGray300AndGray400,
-                shape = RoundedCornerShape(size = 12.dp)
-            )
-            .clip(shape = RoundedCornerShape(size = 12.dp))
-            .combinedClickable(
-                onLongClick = { if(isSelectedMode) shortClick = !shortClick else onSelectHas.invoke(has) },
-                onClick = { if(isSelectedMode) onSelectHas.invoke(has) else shortClick = !shortClick },
-            ),
-        contentAlignment = Alignment.Center
-    ) {
-        SubcomposeAsyncImage(
-            modifier = Modifier
-                .aspectRatio(158f / 210f)
-                .fillMaxSize()
-                .alpha(if (shortClick) 0.4f else 1f),
-            loading = { ShimmerLoadingAnimation() },
-            error = { ShimmerLoadingAnimation() },
-            contentScale = ContentScale.Crop,
-            model = has.imagePath, // 엑박 이미지 필요?
-            contentDescription = "has image"
-        )
+    singleClick { singleClickEventListener ->
 
-        if(shortClick) {
-            FlowRow(
-                modifier = Modifier.padding(top = 17.dp, bottom = 17.dp, start = 15.dp, end = 15.dp),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                tagList.forEach { tag ->
-                    TagHasChipItem(
-                        name = tag.name,
-                        isSelected = tag.checkSelected(selectTagList)
-                    )
-                }
-            }
-        }
-    }
-
-    if(isSelectedMode) {
         Box(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(12.dp),
-            contentAlignment = Alignment.TopEnd
+                .border(
+                    width = if (selectedNumber.isNotNull()) 2.dp else 1.dp,
+                    color = if (selectedNumber.isNotNull()) Purple else ColorFamilyGray300AndGray400,
+                    shape = RoundedCornerShape(size = 12.dp)
+                )
+                .clip(shape = RoundedCornerShape(size = 12.dp))
+                .combinedClickable(
+                    onLongClick = {
+                        singleClickEventListener.onClick {
+                            if (isSelectedMode) shortClick =
+                                !shortClick else onSelectHas.invoke(has)
+                        }
+                    },
+                    onClick = {
+                        singleClickEventListener.onClick {
+                            if (isSelectedMode) onSelectHas.invoke(has) else shortClick =
+                                !shortClick
+                        }
+                    },
+                ),
+            contentAlignment = Alignment.Center
         ) {
-            Box(
+            SubcomposeAsyncImage(
                 modifier = Modifier
-                    .size(18.dp)
-                    .clickable { onSelectHas.invoke(has) }
-                ,
-                contentAlignment = Alignment.Center
-            ) {
-                if(selectedNumber.isNotNull()) {
-                    Image(
-                        painter = painterResource(id = R.drawable.btn_purple_select),
-                        contentDescription = "has select"
-                    )
-                    HasText(
-                        text = selectedNumber.toString(),
-                        color = White,
-                        fontSize = 12.dp,
-                        fontWeight = HasFontWeight.Bold
-                    )
-                } else {
-                    Image(
-                        painter = painterResource(id = R.drawable.btn_purple_no_select),
-                        contentDescription = "has select"
-                    )
+                    .aspectRatio(158f / 210f)
+                    .fillMaxSize()
+                    .alpha(if (shortClick) 0.4f else 1f),
+                loading = { ShimmerLoadingAnimation() },
+                error = { ShimmerLoadingAnimation() },
+                contentScale = ContentScale.Crop,
+                model = has.imagePath, // 엑박 이미지 필요?
+                contentDescription = "has image"
+            )
+
+            if(shortClick) {
+                FlowRow(
+                    modifier = Modifier.padding(top = 17.dp, bottom = 17.dp, start = 15.dp, end = 15.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    tagList.forEach { tag ->
+                        TagHasChipItem(
+                            name = tag.name,
+                            isSelected = tag.checkSelected(selectTagList)
+                        )
+                    }
                 }
             }
         }
+
+        if(isSelectedMode) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.TopEnd
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(18.dp)
+                        .clickable { singleClickEventListener.onClick { onSelectHas.invoke(has) } }
+                    ,
+                    contentAlignment = Alignment.Center
+                ) {
+                    if(selectedNumber.isNotNull()) {
+                        Image(
+                            painter = painterResource(id = R.drawable.btn_purple_select),
+                            contentDescription = "has select"
+                        )
+                        HasText(
+                            text = selectedNumber.toString(),
+                            color = White,
+                            fontSize = 12.dp,
+                            fontWeight = HasFontWeight.Bold
+                        )
+                    } else {
+                        Image(
+                            painter = painterResource(id = R.drawable.btn_purple_no_select),
+                            contentDescription = "has select"
+                        )
+                    }
+                }
+            }
+        } else {
+            Balloon(
+                modifier = Modifier
+                    .padding(end = 10.dp),
+                builder = builder,
+                balloonContent = {
+                    Box(
+                        contentAlignment = Alignment.TopEnd
+                    ) {
+                        Box(modifier = Modifier
+                            .size(80.dp)
+                            .background(Green)) {
+                            Log.d("qwe123", "HasBalloon")
+                            HasText(
+                                text = "수정",
+                                color = Black
+                            )
+                            HasText(
+                                text = "수정",
+                                color = Black
+                            )
+                        }
+                    }
+                }, content = { balloonWindow ->
+                    var size by remember { mutableStateOf(IntSize.Zero) }
+                    var position by remember { mutableStateOf(Offset.Zero) }
+
+                    LaunchedEffect(balloonEvent) {
+                        when (balloonEvent) {
+                            TimeLineMenuEvent.NONE -> Unit
+                            TimeLineMenuEvent.CLOSE -> balloonWindow.dismiss()
+                            TimeLineMenuEvent.OPEN -> balloonWindow.showAsDropDown(
+//                                xOff = (position.x * 0.55).toInt(),
+                                yOff = size.height * 2
+                            )
+                        }
+                    }
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(top = 4.dp),
+                        contentAlignment = Alignment.TopEnd
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .padding(6.dp)
+                                .clickableNoRipple {
+                                    singleClickEventListener.onClick {
+                                        balloonEvent = TimeLineMenuEvent.OPEN
+                                    }
+                                }
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.btn_more_light),
+                                contentDescription = "feed more"
+                            )
+                        }
+                    }
+                }
+            )
+        }
     }
+}
+
+internal enum class TimeLineMenuEvent {
+    NONE, OPEN, CLOSE
 }
