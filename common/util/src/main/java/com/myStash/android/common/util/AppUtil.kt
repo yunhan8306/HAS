36c11d924b2e3d2bb2a224cf81f8055ef05a3758
yunhan8306 <dbsgks17@naver.com>
2024-09-08 01:25:11 +0900

    Feat: get app version

---
+++ common/util/src/main/java/com/myStash/android/common/util/AppUtil.kt
@@ -0,0 +1,11 @@
+package com.myStash.android.common.util
+
+import android.content.Context
+
+fun Context.getAppVersion(): String? {
+    return try {
+        this.packageManager.getPackageInfo(this.packageName, 0).versionName
+    } catch (e: Exception) {
+        null
+    }
+}
